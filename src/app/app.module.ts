6697b5c8dc89d39853ac0ab7cd9d5ce07a2ab6cf
Daniel Dudas <danimdy@gmail.com>
2017-09-09 11:34:59 +0300

    add components that we will need for user interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChartComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
